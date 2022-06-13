823a11a3f5fb6ba1dc4550c35971882120d536ea
tarique vieira ramos <tariquevieira@gmail.com>
2022-06-13 10:23:53 -0300

    feat(filtro): add filtro

---
+++ src/pages/Cardapio/Buscador/index.tsx
@@ -0,0 +1,20 @@
+import styles from './Buscador.module.scss';
+import { CgSearch } from 'react-icons/cg';
+
+interface Props {
+  busca: string;
+  setBusca: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function Buscador(props: Props) {
+  return (
+    <div className={styles.buscador}>
+      <input
+        type="text"
+        value={props.busca}
+        onChange={(evento) => props.setBusca(evento.target.value)}
+      />
+      <CgSearch size={20} color="#4c4d5e" />
+    </div>
+  );
+}
--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,7 +1,12 @@
 import styles from './Cardapio.module.scss';
 import { ReactComponent as Logo } from 'assets/img/logo.svg';
+import Buscador from './Buscador';
+import { useState } from 'react';
+import Filtros from './Filtros';
 
 export default function Cardapio() {
+  const [busca, setBusca] = useState('');
+  const [filtro, setFiltro] = useState<number | null>(null);
   return (
     <main>
       <nav className={styles.menu}>
@@ -10,6 +15,13 @@ export default function Cardapio() {
       <header className={styles.header}>
         <div className={styles.header__text}>A casa do código e da massa!</div>
       </header>
+      <section className={styles.cardapio}>
+        <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+        <Buscador busca={busca} setBusca={setBusca} />
+        <div className={styles.cardapio__filtro}>
+          <Filtros filtro={filtro} setFiltro={setFiltro} />
+        </div>
+      </section>
     </main>
   );
 }
